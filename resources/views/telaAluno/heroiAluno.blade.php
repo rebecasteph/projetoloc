53d86dd42c6db21a6d6c3a135c43d48c748feb40
Rebeca Stephanie <02360308254@saude.am.gov.br>
2020-08-20 17:23:18 -0400

    painel pontos aluno

--- resources/views/telaAluno/heroiAluno.blade.php
+++ resources/views/telaAluno/heroiAluno.blade.php
@@ -6,13 +6,27 @@
 <div class="d-flex flex-md-row flex-column">
 
     <div class="col-md-4">
-        <div class="">
-            <img src="" alt="">
-            <h6>Nome do Time</h6>
+        <div>
+            <img class="escudo-elo-sm d-inline" src="" alt="elo">
+            <h6 class="d-inline">Nome do Time</h6>
         </div>
-        <h4>Nome do Aluno</h4>
+        <div class="">
+            <h3>Nome do Aluno</h3>
+            <span class="badge badge-pill badge-dark">Nível 2</span>
+            <span class="badge badge-pill badge-warning">Nível Equipe</span>
+        </div>
+        <div class="xp">
+            <h3 class="d-inline-flex">XP</h3>
+            <div class="d-inline-flex progress progress-bar-size m-2">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style="width:25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                    <!-- <span class="text-dark ml-1"><strong>25%</strong></span> -->
+                </div>
+            </div>
+            <h3 class="pb">PB 999</h3>
+
+        </div>
     </div>
-    <div class="col-md-8">
-        ola
+    <div class="col-md-8" style="background-color: #cacaca; height: 600px;">
+        <img class="" src="" alt="painel-do-heroi">
     </div>
 </div>
