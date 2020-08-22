d786107b90fbc4524fcfe36ff4ec3bf2c5f27c5d
Rebeca Assunção <rsva.lic@uea.edu.br>
2020-08-22 00:56:42 -0400

    começo tela jogadores

--- resources/views/telaAluno/jogadoresAluno.blade.php
+++ resources/views/telaAluno/jogadoresAluno.blade.php
@@ -1,15 +1,41 @@
 
-<ul class="nav nav-sec nav-pills justify-content-center">
+
+<ul class="nav-sec justify-content-center nav nav-pills" id="pills-tab" role="tablist">
   <li class="nav-item">
-    <a class="nav-link active" href="#">Ativo</a>
+    <a class="nav-link active" id="jogadores-tab" data-toggle="pill" href="#jogadores" role="tab" aria-controls="jogadores" aria-selected="true">Jogadores</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" id="equipe-tab" data-toggle="pill" href="#equipe" role="tab" aria-controls="equipe" aria-selected="false">Equipe</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#">Desativado</a>
+    <a class="nav-link" id="turma-tab" data-toggle="pill" href="#turma" role="tab" aria-controls="turma" aria-selected="false">Turma</a>
   </li>
 </ul>
+<div class="tab-content" id="pills-tabContent">
+
+  <!-- JOGADORES -->
+  <div class="tab-pane fade show active" id="jogadores" role="tabpanel" aria-labelledby="jogadores-tab">
+    <div class=" mb-1">
+        <img class="escudo-elo-md" src="{!!asset('img/elos/elo_Epico.png')!!}" alt="elo">
+        <h3 class=" pt-3">Nome do Time</h3>
+    </div>
+
+
+
+
+
+  </div>
+
+
+
+  <!-- EQUIPES -->
+  <div class="tab-pane fade" id="equipe" role="tabpanel" aria-labelledby="equipe-tab">
+    <h3>equipe</h3>
+  </div>
+
+  <!-- TURMA -->
+  <div class="tab-pane fade" id="turma" role="tabpanel" aria-labelledby="turma-tab">
+    <h3>turma</h3>
+  </div>
+</div>
+
