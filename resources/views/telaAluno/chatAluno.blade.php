82c554d2024c1d1c930bf5d1a6d62b94948099a2
Rebeca Stephanie <02360308254@saude.am.gov.br>
2020-09-01 16:13:04 -0400

    nav chat comunicados e prof

--- resources/views/telaAluno/chatAluno.blade.php
+++ resources/views/telaAluno/chatAluno.blade.php
@@ -2,124 +2,101 @@
 
 <div class="inbox_msg">
     <div class="inbox_people">
-        <div class="inbox_chat">
-            <div class="chat_list active_chat">
+
+        <div class="inbox_chat nav nav-tab" role="tablist">
+
+            <div class="nav-link chat_list active" data-toggle="tab" id="comunicAluno-tab" href="#comunicAluno" role="tab" aria-controls="comunicAluno" aria-selected="true">
                 <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p> 
-                </div>
+                    <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                    <div class="chat_ib">
+                        <h5>Comunicados <span class="chat_date">Dec 25</span></h5>
+                        <p>Último comunicado publicado pelo profesor.</p> 
+                    </div>
                 </div>
             </div>
-            <div class="chat_list">
+            <div class="nav-link chat_list" data-toggle="tab" id="chatProf-tab" href="#chatProf" role="tab" aria-controls="chatProf" aria-selected="false">
                 <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
+                    <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                    <div class="chat_ib">
+                        <h5>Professor <span class="chat_date">Dec 25</span></h5>
+                        <p>Última mensagem enviada ou recebida.</p>
+                    </div>
                 </div>
             </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-                </div>
-            </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-                </div>
-            </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-                </div>
-            </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-                </div>
-            </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-                </div>
-            </div>
+
         </div>
     </div>
+
     <div class="mesgs tela-dir">
-        <div class="msg_history">
-            <div class="incoming_msg">
-                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="received_msg">
-                <div class="received_withd_msg">
+        <div class="tab-content" >
+
+            <div class="tab-pane fade show active" id="comunicAluno" role="tabpanel" aria-labelledby="comunicAluno-tab">
+
+                <div class="incoming_msg">
+                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                    <div class="received_msg">
+                    <div class="received_withd_msg">
+                        <p>COMUNICADO: Lorem ipsum laoreet massa arcu vulputate justo nibh, litora nullam consequat porttitor urna senectus pretium, aptent mattis aliquam per donec porta. fringilla nullam curabitur aliquam metus quis curae blandit, sagittis tellus leo potenti dictum suscipit sapien purus, ultricies praesent sit vivamus a erat. eget donec dictum elementum tellus consectetur nullam, ante suscipit eros nibh leo gravida, velit curabitur malesuada consequat non. in tellus molestie habitant per gravida quam neque condimentum, egestas curabitur enim conubia egestas aptent enim, purus fames nam erat magna scelerisque aliquam. sagittis suspendisse vulputate dolor lobortis condimentum fames scelerisque ac sem nullam diam nec lorem mollis, dolor class tortor dictum ligula dui semper donec cras turpis phasellus est.</p>
+                        <span class="time_date"> 11:01 AM    |    June 9</span></div>
+                    </div>
+                </div>
+                <div class="incoming_msg">
+                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                    <div class="received_msg">
+                    <div class="received_withd_msg">
+                        <p>Lorem ipsum laoreet massa arcu vulputate justo nibh, litora nullam consequat porttitor urna senectus pretium, aptent mattis aliquam per donec porta. fringilla nullam curabitur aliquam metus quis curae blandit, sagittis tellus leo potenti dictum suscipit sapien purus, ultricies praesent sit vivamus a erat.</p>
+                        <span class="time_date"> 11:01 AM    |    June 9</span></div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="tab-pane fade" id="chatProf" role="tabpanel" aria-labelledby="chatProf-tab">
+                <div class="incoming_msg">
+                    <div class="incoming_msg_img"><img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                    <div class="received_msg">
+                    <div class="received_withd_msg">
+                        <p>oi prof</p>
+                        <span class="time_date"> 11:01 AM    |    June 9</span></div>
+                    </div>
+                </div>
+                <div class="outgoing_msg">
+                    <div class="sent_msg">
                     <p>Test which is a new approach to have all
-                    solutions</p>
-                    <span class="time_date"> 11:01 AM    |    June 9</span></div>
+                        solutions</p>
+                    <span class="time_date"> 11:01 AM    |    June 9</span> </div>
                 </div>
-            </div>
-            <div class="outgoing_msg">
-                <div class="sent_msg">
-                <p>Test which is a new approach to have all
-                    solutions</p>
-                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
-            </div>
-            <div class="incoming_msg">
-                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="received_msg">
-                <div class="received_withd_msg">
-                    <p>Test, which is a new approach to have</p>
-                    <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
+                <div class="incoming_msg">
+                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                    <div class="received_msg">
+                    <div class="received_withd_msg">
+                        <p>Test, which is a new approach to have</p>
+                        <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
+                    </div>
                 </div>
-            </div>
-            <div class="outgoing_msg">
-                <div class="sent_msg">
-                <p>Apollo University, Delhi, India Test</p>
-                <span class="time_date"> 11:01 AM    |    Today</span> </div>
-            </div>
-            <div class="incoming_msg">
-                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="received_msg">
-                <div class="received_withd_msg">
-                    <p>We work directly with our designers and suppliers,
-                    and sell direct to you, which means quality, exclusive
-                    products, at a price anyone can afford.</p>
-                    <span class="time_date"> 11:01 AM    |    Today</span></div>
+                <div class="outgoing_msg">
+                    <div class="sent_msg">
+                    <p>Apollo University, Delhi, India Test</p>
+                    <span class="time_date"> 11:01 AM    |    Today</span> </div>
                 </div>
-            </div>
-        </div>
-        <div class="type_msg">
-            <div class="input_msg_write">
-                <input type="text" class="write_msg" placeholder="Escreva aqui..." />
-                <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                <div class="incoming_msg">
+                    <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                    <div class="received_msg">
+                    <div class="received_withd_msg">
+                        <p>We work directly with our designers and suppliers,
+                        and sell direct to you, which means quality, exclusive
+                        products, at a price anyone can afford.</p>
+                        <span class="time_date"> 11:01 AM    |    Today</span></div>
+                    </div>
+                </div>
+
+                <div class="type_msg">
+                    <div class="input_msg_write">
+                        <input type="text" class="write_msg" placeholder="Escreva aqui..." />
+                        <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
