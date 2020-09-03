1f7c454a2d3799985195b4ccf7dd59f4dcaeedc8
Rebeca Stephanie <02360308254@saude.am.gov.br>
2020-09-03 14:48:15 -0400

    tela lista missões

--- resources/views/telaAluno/missao/missaoAluno.blade.php
+++ resources/views/telaAluno/missao/missaoAluno.blade.php
@@ -1,10 +1,85 @@
-<div class="container">
-    <article class="d-flex flex-md-row flex-column tile is-child notification is-danger w-75">
-        <div class="col-9">
-            <h5 class="card-title">Título do card</h5>
-            <p class="card-text">Com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>        </div>
-        <div class="col-3  caixa-missao">
-            <button type="button" class="btn btn-light">Dark</button>
+<div class="container w-75" style="margin: 0 auto;">
+
+    <br>
+    <div class=" card p-0 tile is-child notification is-link">
+        <div class="card-header d-flex flex-md-row flex-column">
+            <div class="col-11">
+                <h5 class="card-title mb-2">Nome Turma "1oA"</h5>
+                <span>Nome Instituição</span>
+                <!--<button type="button" class="pt-0 pb-0 btn btn-light">
+                    <span class="fas fa-user-friends"> <span class="badge badge-light">17</span>
+                    <span class="fas fa-clipboard"> <span class="badge badge-light">4</span>
+                </button> -->
+            </div>
+            <div class="col-1 caixa-missao">
+                <button type="button" class="btn btn-light" style="justify-content: center;"><span class="fas fa-cog"></span></button>
+            </div>
         </div>
-    </article>
+    </div>
+    <br>
+    <div class=" card p-0 tile is-child notification is-warning">
+        <div class="card-header d-flex flex-md-row flex-column">
+            <div class="col-11">
+                <h5 class="card-title mb-2">Nome Turma "1oB"</h5>
+                <span>Nome Instituição</span>
+                <!--<button type="button" class="pt-0 pb-0 btn btn-light">
+                    <span class="fas fa-user-friends"> <span class="badge badge-light">17</span>
+                    <span class="fas fa-clipboard"> <span class="badge badge-light">4</span>
+                </button> -->
+            </div>
+            <div class="col-1 caixa-missao">
+                <button type="button" class="btn btn-light" style="justify-content: center;"><span class="fas fa-cog"></span></button>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class=" card p-0 tile is-child notification is-success">
+        <div class="card-header d-flex flex-md-row flex-column">
+            <div class="col-11">
+                <h5 class="card-title mb-2">Nome Turma "1oC"</h5>
+                <span>Nome Instituição</span>
+                <!--<button type="button" class="pt-0 pb-0 btn btn-light">
+                    <span class="fas fa-user-friends"> <span class="badge badge-light">17</span>
+                    <span class="fas fa-clipboard"> <span class="badge badge-light">4</span>
+                </button> -->
+            </div>
+            <div class="col-1 caixa-missao">
+                <button type="button" class="btn btn-light" style="justify-content: center;"><span class="fas fa-cog"></span></button>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class=" card p-0 tile is-child notification is-primary">
+        <div class="card-header d-flex flex-md-row flex-column">
+            <div class="col-11">
+                <h5 class="card-title mb-2">Nome Turma "1oD"</h5>
+                <span>Nome Instituição</span>
+                <!--<button type="button" class="pt-0 pb-0 btn btn-light">
+                    <span class="fas fa-user-friends"> <span class="badge badge-light">17</span>
+                    <span class="fas fa-clipboard"> <span class="badge badge-light">4</span>
+                </button> -->
+            </div>
+            <div class="col-1 caixa-missao">
+                <button type="button" class="btn btn-light" style="justify-content: center;"><span class="fas fa-cog"></span></button>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class=" card p-0 tile is-child notification is-danger">
+        <div class="card-header d-flex flex-md-row flex-column">
+            <div class="col-11">
+                <h5 class="card-title mb-2">Nome Turma "1oE"</h5>
+                <span>Nome Instituição</span>
+                <!--<button type="button" class="pt-0 pb-0 btn btn-light">
+                    <span class="fas fa-user-friends"> <span class="badge badge-light">17</span>
+                    <span class="fas fa-clipboard"> <span class="badge badge-light">4</span>
+                </button> -->
+            </div>
+            <div class="col-1 caixa-missao">
+                <button type="button" class="btn btn-light" style="justify-content: center;"><span class="fas fa-cog"></span></button>
+            </div>
+        </div>
+    </div>
+
+
 </div>
