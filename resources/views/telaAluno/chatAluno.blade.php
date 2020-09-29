96a7b6406bf88012c13d47482fa64091a079e04a
Rebeca Assunção <rsva.lic@uea.edu.br>
2020-09-29 00:32:58 -0400

    ajustes cor

--- resources/views/telaAluno/chatAluno.blade.php
+++ resources/views/telaAluno/chatAluno.blade.php
@@ -90,10 +90,10 @@
                     </div>
                 </div>
 
-                <div class="type_msg">
-                    <div class="input_msg_write">
-                        <input type="text" class="write_msg" placeholder="Escreva aqui..." />
-                        <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="escrever" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-roxo" type="button" id="button-addon2">Enviar</button>
                     </div>
                 </div>
 
