6e2960119083f2a05f64008538dee65d90148fa6
Rebeca Stephanie <02360308254@saude.am.gov.br>
2020-09-29 16:52:16 -0400

    painel questao fase chefao

--- resources/views/telaAluno/chefao/faseGeral.blade.php
+++ resources/views/telaAluno/chefao/faseGeral.blade.php
@@ -3,19 +3,61 @@
  @section('title', 'Nome Fase do Chefão')
    
  @section('content')
- <div class="nav nav-sec" aria-label="breadcrumb" style="width: 100%">
+ <div class="nav nav-sec" aria-label="breadcrumb">
      <p>Nome da Turma</p>
      <span>/Fase do Chefão</span>
      <span>/Nome do Desafio</span>
  </div>
- <div class="container-fluid">
+ <div class="area-fase-chefao">
 
      <!-- Botão para voltar -->
      <a href="{{ url('/inicial') }}">
         <button type="button" class="btn btn-roxo"><span class="fas fa-angle-double-left"></span></button>
      </a> 
 
+    <!-- Botão para acionar modal -->
+    <button type="button" class="btn btn-roxo" data-toggle="modal" data-target="#quest-fase">
+        Abrir modal de demonstração
+    </button>
 
+    <!-- Modal -->
+    <div class="modal fade" id="quest-fase" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                    <div class="col-auto">
+                        <h5 class="modal-title">Nome da Fase</h5>
+                        <div class="row show-moblie">
+                            <div class="col-auto tags has-addons mb-0">
+                                <span class="tag is-dark mb-0">XP</span>
+                                <span class="tag is-warning mb-0">33</span>
+                            </div>
+                            <div class="col-auto tags has-addons mb-0">
+                                <span class="tag is-dark mb-0">HP</span>
+                                <span class="tag is-danger mb-0">15</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <div class="row justify-content-end">
+                            <div class="col-auto tags has-addons mb-0 none-mobile">
+                                <span class="tag is-dark mb-0">PB</span>
+                                <span class="tag is-warning mb-0">33</span>
+                            </div>
+                            <div class="col-auto tags has-addons mb-0 none-mobile">
+                                <span class="tag is-dark mb-0">XP</span>
+                                <span class="tag is-danger mb-0">15</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                @include('telaAluno.chefao.painelQuestFase')
+            </div>
+        </div>
+    </div>
  
  </div>
   
---
+++ resources/views/telaAluno/chefao/painelQuestFase.blade.php
@@ -0,0 +1,34 @@
+<div class="modal-body pr-4 pl-4">
+    <h5 class="text-center pt-2">Questão simples relacionada aos conteúdos vistos em aula e em Missões (?) </h5>
+    <br>
+
+    <div>
+        <div class="custom-control custom-radio">
+            <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
+            <label class="custom-control-label" for="customRadio1">Alternativa 1</label>
+        </div>
+
+        <div class="custom-control custom-radio">
+            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
+            <label class="custom-control-label" for="customRadio2">Alternativa 2</label>
+        </div>
+
+        <div class="custom-control custom-radio">
+            <input type="radio" id="customRadio3" name="customRadio" class="custom-control-input">
+            <label class="custom-control-label" for="customRadio3">Alternativa 3</label>
+        </div>
+
+        <div class="custom-control custom-radio">
+            <input type="radio" id="customRadio4" name="customRadio" class="custom-control-input">
+            <label class="custom-control-label" for="customRadio4">Alternativa 4</label>
+        </div>
+
+
+    </div>
+    
+</div>
+
+<div class="modal-footer">
+    <button type="button" class="btn" data-dismiss="modal">Desistir</button>
+    <button type="button" class="btn btn-roxo">Responder</button>
+</div>
