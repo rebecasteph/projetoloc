460f8851da18d8bee7d87126e0b36f7a03add7f1
Rebeca Assunção <rsva.lic@uea.edu.br>
2020-10-04 13:28:17 -0400

    tela lista turmas sem rota

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -3,19 +3,16 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class=" collapse navbar-collapse my-1 justify-content-end" id="navbarNav">
+    <div class="collapse navbar-collapse my-1 justify-content-end" id="navbarNav">
         <ul class="navbar-nav align-self-end">
             <li class="nav-item active">
-                <a class="nav-link" href="\">Home <span class="sr-only">(Página atual)</span></a>
+                <a class="nav-link" href="#">Turmas</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Destaques</a>
+                <a class="nav-link" href="#">Perfil</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Preços</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Desativado</a>
+                <a class="nav-link disabled" href="#">Sair</a>
             </li>
         </ul>
     </div>
---
+++ resources/views/telaAluno/listaTurmas.blade.php
@@ -0,0 +1,15 @@
+<div class="container w-75" style="margin: 0 auto;" >
+
+    <br>
+    <div class="card">
+        <h5 class="card-header roxo"><span>Nome Turma</span></h5>
+        <div class="card-body text-center">
+            <p class="card-text">Com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+            <a href="missao-aluno/nome-missao" class="button is-primary is-outlined">Visitar</a>
+        </div>
+    </div>
+
+
+
+
+</div>
