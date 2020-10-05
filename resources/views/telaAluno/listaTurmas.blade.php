a42f16ebf17e18c9ff32ddfc2fb037bf6f54a95b
Rebeca Assunção <rsva.lic@uea.edu.br>
2020-10-04 22:35:34 -0400

    tela lista de turmas

--- resources/views/telaAluno/listaTurmas.blade.php
+++ resources/views/telaAluno/listaTurmas.blade.php
@@ -1,15 +1,42 @@
-<div class="container w-75" style="margin: 0 auto;" >
+@extends('template')
+ 
+@section('title', 'Minhas Turmas')
+  
+@section('content')
+
+<div class="container w-75 tab-pane-tela" style="margin: 0 auto;" >
 
     <br>
-    <div class="card">
-        <h5 class="card-header roxo"><span>Nome Turma</span></h5>
-        <div class="card-body text-center">
+    <div class="card text-center">
+        <div class="card-header roxo"><span>Nome da Instituição</span></div>
+        <div class="card-body">
+            <h5 class="card-title">Nome da Turma</h5>
             <p class="card-text">Com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
-            <a href="missao-aluno/nome-missao" class="button is-primary is-outlined">Visitar</a>
+            <a href="inicial" class="button btn-roxo">Visitar</a>
+        </div>
+    </div>
+
+    <br>
+    <div class="card text-center">
+        <div class="card-header roxo"><span>Nome da Instituição</span></div>
+        <div class="card-body">
+            <h5 class="card-title">Nome da Turma</h5>
+            <p class="card-text">Com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+            <a href="inicial" class="button btn-roxo">Visitar</a>
+        </div>
+    </div>
+
+    <br>
+    <div class="card text-center">
+        <div class="card-header roxo"><span>Nome da Instituição</span></div>
+        <div class="card-body">
+            <h5 class="card-title">Nome da Turma</h5>
+            <p class="card-text">Com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+            <a href="inicial" class="button btn-roxo">Visitar</a>
         </div>
     </div>
 
 
 
-
 </div>
+@endsection
