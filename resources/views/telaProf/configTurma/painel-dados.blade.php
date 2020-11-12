661beeb897c814179ad7ddaf8dbb0dfe3a96a10f
Rebeca Assunção <rsva.lic@uea.edu.br>
2020-11-12 00:02:24 -0400

    campo dados da turma

---
+++ resources/views/telaProf/configTurma/painel-dados.blade.php
@@ -0,0 +1,38 @@
+<div class="pt-4">
+    <form class="needs-validation" novalidate>
+        <div class="form-row mb-3">
+            <div class="col-md-3">
+                <label>Código da Turma: <strong>XXX123Abc</strong></label>
+            </div>
+            <div class="col-md-4">
+                <article class="message is-small is-info">
+                    <div class="message-body p-2">
+                        Copie e compartilhe o código com seus alunos.
+                    </div>
+                </article>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="col-md-7 mb-3">
+                <label>Nome da turma</label>
+                <input type="text" class="form-control" id="nome-turma" placeholder="Nome da Turma" value="Nome" required>
+                <div class="valid-feedback">
+                    Tudo certo!
+                </div>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="col-md-7 mb-3">
+                <label>Instituição vinculada</label>
+                <input type="text" class="form-control" id="nome-instituicao" placeholder="Instituição" required>
+                <div class="invalid-feedback">
+                    Por favor, informe um e-mail válido.
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="d-flex justify-content-center">
+            <button class="btn btn-roxo" type="submit">Enviar</button>
+        </div>
+    </form>
+</div>
